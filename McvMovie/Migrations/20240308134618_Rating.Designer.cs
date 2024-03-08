2803c3a8425075f8b4e91eb2b2ba1c2b433cb08b
NemanjaCyber <neca.s@elfak.rs>
2024-03-08 14:50:57 +0100

    Rating

--- McvMovie/Migrations/20240308134618_Rating.Designer.cs
+++ McvMovie/Migrations/20240308134618_Rating.Designer.cs
@@ -4,6 +4,7 @@ using McvMovie.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace McvMovie.Migrations
 {
     [DbContext(typeof(McvMovieContext))]
-    partial class McvMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20240308134618_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
